2881f6271ba4a32713966add80973b66e4008917
thanhba0505 <lethanhba0505@gmail.com>
2026-06-29 09:01:53 +0700

    feat(cors): add and configure rack-cors for CORS support
    configure dynamic CORS origin checking: use CORS_ORIGINS environment variable for allowed origins in production, allow localhost and 127.0.0.1 for development and test environments, expose Authorization header for client applications, set 10-minute preflight request cache max age, and permit all standard HTTP methods and any request headers

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -5,12 +5,23 @@
 
 # Read more: https://github.com/cyu/rack-cors
 
-# Rails.application.config.middleware.insert_before 0, Rack::Cors do
-#   allow do
-#     origins "example.com"
-#
-#     resource "*",
-#       headers: :any,
-#       methods: [:get, :post, :put, :patch, :delete, :options, :head]
-#   end
-# end
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+  allow do
+    origins do |origin, _env|
+      next false if origin.blank?
+
+      if Rails.env.production?
+        allowed = ENV.fetch("CORS_ORIGINS", "").split(",").map(&:strip).reject(&:blank?)
+        allowed.include?(origin)
+      else
+        origin.match?(/\Ahttps?:\/\/(localhost|127\.0\.0\.1)(:\d+)?\z/)
+      end
+    end
+
+    resource "*",
+      headers: :any,
+      expose: %w[Authorization],
+      methods: %i[get post put patch delete options head],
+      max_age: 600
+  end
+end
